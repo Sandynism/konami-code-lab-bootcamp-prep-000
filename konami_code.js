33caf91ab5bb276ed80115d93746728e98811fd0
Sandy Yeung <sandynism@gmail.com>
2017-09-30 18:45:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0
+
 function init() {
 
-var index = 0
 
 function onKeyDownHandler(e) {
   document.body.addEventListener('keydown', function(e) {
