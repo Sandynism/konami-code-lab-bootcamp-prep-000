982382e929af07ee1bd734a3fe5e5e9612eba108
Sandy Yeung <sandynism@gmail.com>
2017-09-30 18:44:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
 var index = 0
 
 function onKeyDownHandler(e) {
-  document.body.addEventListening('keydown', function(e) {
+  document.body.addEventListener('keydown', function(e) {
   const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
