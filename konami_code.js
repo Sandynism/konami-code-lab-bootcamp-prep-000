901cd863fdb16fe2eb3a5157cfa6388716983bb7
Sandy Yeung <sandynism@gmail.com>
2017-09-30 18:42:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
 var index = 0
 
 function onKeyDownHandler(e) {
-  document.body.addEventListening('keydown', function(e) {
+  document.body.addEventListening('keydown', function(e))
   const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
@@ -19,5 +19,5 @@ function onKeyDownHandler(e) {
   } else {
     index = 0;
   }
-});
+}
 }
